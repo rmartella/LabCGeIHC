d3f0a06fb7dabd5d077e9bb5b268023f8aa8ee20
rmartella <reymar_44@hotmail.com>
2019-02-11 00:11:06 -0600

    Change of the rmartella

--- Practica3/08-SistemasCoordenadas/src/main.cpp
+++ Practica3/08-SistemasCoordenadas/src/main.cpp
@@ -238,8 +238,8 @@ void applicationLoop() {
 
 	while (psi) {
 		psi = processInput(true);
+		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
 		shader.turnOn();
