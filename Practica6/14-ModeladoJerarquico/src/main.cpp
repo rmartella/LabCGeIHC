e24aebae62b3431ea597760cd924bfc50197c85c
rmartella <reymar_44@hotmail.com>
2019-03-11 04:41:51 -0600

    Add the seasson practical 6 and 7

---
+++ Practica6/14-ModeladoJerarquico/src/main.cpp
@@ -0,0 +1,302 @@
+//glew include
+#include <GL/glew.h>
+
+//std includes
+#include <string>
+#include <iostream>
+
+//glfw include
+#include <GLFW/glfw3.h>
+
+//GLM include
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+// program include
+#include "Headers/TimeManager.h"
+#include "Headers/Shader.h"
+// Geometry primitives
+#include "Headers/Sphere.h"
+#include "Headers/Cylinder.h"
+#include "Headers/Box.h"
+
+Sphere sphere(20, 20);
+Cylinder cylinder(20, 20, 0.5, 0.5);
+Cylinder cylinder2(20, 20, 0.5, 0.5);
+Box box;
+
+Shader shader;
+
+int screenWidth;
+int screenHeight;
+
+GLFWwindow * window;
+
+bool exitApp = false;
+int lastMousePosX;
+int lastMousePosY;
+
+double deltaTime;
+
+// Se definen todos las funciones.
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes);
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod);
+void init(int width, int height, std::string strTitle, bool bFullScreen);
+void destroyWindow();
+void destroy();
+bool processInput(bool continueApplication = true);
+
+// Implementacion de todas las funciones.
+void init(int width, int height, std::string strTitle, bool bFullScreen) {
+
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		exit(-1);
+	}
+
+	screenWidth = width;
+	screenHeight = height;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	if (bFullScreen)
+		window = glfwCreateWindow(width, height, strTitle.c_str(),
+			glfwGetPrimaryMonitor(), nullptr);
+	else
+		window = glfwCreateWindow(width, height, strTitle.c_str(), nullptr,
+			nullptr);
+
+	if (window == nullptr) {
+		std::cerr
+			<< "Error to create GLFW window, you can try download the last version of your video card that support OpenGL 3.3+"
+			<< std::endl;
+		destroyWindow();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(0);
+
+	glfwSetWindowSizeCallback(window, reshapeCallback);
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Init glew
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		std::cerr << "Failed to initialize glew" << std::endl;
+		exit(-1);
+	}
+
+	glViewport(0, 0, screenWidth, screenHeight);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	glEnable(GL_DEPTH_TEST);
+
+	shader.initialize("../../Shaders/transformaciones.vs", "../../Shaders/transformaciones.fs");
+
+	sphere.init();
+	sphere.setShader(&shader);
+	sphere.setColor(glm::vec3(0.3, 0.3, 1.0));
+
+	cylinder.init();
+	cylinder.setShader(&shader);
+	cylinder.setColor(glm::vec3(0.8, 0.3, 1.0));
+
+	cylinder2.init();
+	cylinder2.setShader(&shader);
+	cylinder2.setColor(glm::vec3(0.2, 0.7, 0.3));
+
+	box.init();
+	box.setShader(&shader);
+	box.setColor(glm::vec3(0.2, 0.8, 0.4));
+
+}
+
+void destroyWindow() {
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void destroy() {
+	destroyWindow();
+
+	shader.destroy();
+	sphere.destroy();
+	cylinder.destroy();
+	cylinder2.destroy();
+}
+
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes) {
+	screenWidth = widthRes;
+	screenHeight = heightRes;
+	glViewport(0, 0, widthRes, heightRes);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (action == GLFW_PRESS) {
+		switch (key) {
+		case GLFW_KEY_ESCAPE:
+			exitApp = true;
+			break;
+		}
+	}
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+	lastMousePosX = xpos;
+	lastMousePosY = ypos;
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
+	if (state == GLFW_PRESS) {
+		switch (button) {
+		case GLFW_MOUSE_BUTTON_RIGHT:
+			std::cout << "lastMousePos.y:" << lastMousePosY << std::endl;
+			break;
+		case GLFW_MOUSE_BUTTON_LEFT:
+			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
+			break;
+		case GLFW_MOUSE_BUTTON_MIDDLE:
+			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
+			std::cout << "lastMousePos.y:" << lastMousePosY << std::endl;
+			break;
+		}
+	}
+}
+
+bool processInput(bool continueApplication) {
+	if (exitApp || glfwWindowShouldClose(window) != 0) {
+		return false;
+	}
+	TimeManager::Instance().CalculateFrameRate(false);
+	deltaTime = TimeManager::Instance().DeltaTime;
+	glfwPollEvents();
+	return continueApplication;
+}
+
+void applicationLoop() {
+	bool psi = true;
+	double lastTime = TimeManager::Instance().GetTime();
+
+	while (psi) {
+		psi = processInput(true);
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+		// Matrix de proyeccion en perspectiva
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f),
+			(float)screenWidth / screenWidth, 0.01f, 100.0f);
+		// matrix de vista
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -8.0f));
+
+		// Matrix con diagonal unitaria
+		// Matriz del Cylindro del torso
+		glm::mat4 matrix0 = glm::mat4(1.0f);
+		// Se coloca el torso en la coordenada (0, 0, -1.0)
+		matrix0 = glm::translate(matrix0, glm::vec3(0.0f, 0.0f, -1.0f));
+		// Matrix de la esfera 1, se coloca -0.5 unidades en el eje y debajo del torso
+		glm::mat4 matrixs1 = glm::translate(matrix0, glm::vec3(0.0f, -0.5f, 0.0f));
+		// Se escala el cylidro del torso
+
+		glm::mat4 matrixs5 = glm::translate(matrix0, glm::vec3(0.0f, 0.5f, 0.0f));
+
+		glm::mat4 matrixs6 = glm::translate(matrixs5, glm::vec3(0.3f, 0.0f, 0.0f));
+
+		glm::mat4 matrix7 = glm::rotate(matrixs6, -0.2f, glm::vec3(0.0f, 0.0f, 1.0f));
+		matrix7 = glm::translate(matrix7, glm::vec3(0.25f, 0.0f, 0.0f));
+		matrix7 = glm::scale(matrix7, glm::vec3(0.5, 0.15, 0.15f));
+		cylinder.setProjectionMatrix(projection);
+		cylinder.setViewMatrix(view);
+		cylinder.enableWireMode();
+		cylinder.setColor(glm::vec3(0.8, 0.3, 1.0));
+		cylinder.render(matrix7);
+
+		matrixs6 = glm::scale(matrixs6, glm::vec3(0.1f, 0.1f, 0.1f));
+		sphere.setProjectionMatrix(projection);
+		sphere.setViewMatrix(view);
+		sphere.enableWireMode();
+		sphere.render(matrixs6);
+
+		matrixs5 = glm::scale(matrixs5, glm::vec3(0.1f, 0.1f, 0.1f));
+		sphere.setProjectionMatrix(projection);
+		sphere.setViewMatrix(view);
+		sphere.enableWireMode();
+		cylinder.setColor(glm::vec3(0.8, 0.3, 1.0));
+		sphere.render(matrixs5);
+
+		matrix0 = glm::scale(matrix0, glm::vec3(0.6f, 1.0f, 0.6f));
+		// Se dibuja el cylindro
+		cylinder.setProjectionMatrix(projection);
+		cylinder.setViewMatrix(view);
+		cylinder.enableWireMode();
+		cylinder.setColor(glm::vec3(0.8, 0.3, 1.0));
+		cylinder.render(matrix0);
+
+		glm::mat4 matrixs2 = glm::translate(matrixs1, glm::vec3(-0.225f, 0.0f, 0.0f));
+		glm::mat4 matrixs3 = glm::translate(matrixs1, glm::vec3(0.225f, 0.0f, 0.0f));
+		matrixs1 = glm::scale(matrixs1, glm::vec3(0.1f, 0.1f, 0.1f));
+		sphere.setProjectionMatrix(projection);
+		sphere.setViewMatrix(view);
+		sphere.enableWireMode();
+		sphere.render(matrixs1);
+
+		glm::mat4 matrix1 = glm::rotate(matrixs2, -0.2f, glm::vec3(0.0f, 0.0f, 1.0f));
+		matrix1 = glm::translate(matrix1, glm::vec3(0.0, -0.4, 0.0));
+
+		glm::mat4 matrixs4 = glm::translate(matrix1, glm::vec3(0.0f, -0.4f, 0.0f));
+
+		glm::mat4 matrix2 = glm::rotate(matrixs4, 0.3f, glm::vec3(0.0f, 0.0f, 1.0f));
+		matrix2 = glm::translate(matrix2, glm::vec3(0.0f, -0.3f, 0.0f));
+		matrix2 = glm::scale(matrix2, glm::vec3(0.1, 0.6, 0.1));
+		cylinder2.setProjectionMatrix(projection);
+		cylinder2.setViewMatrix(view);
+		cylinder2.enableWireMode();
+		cylinder.setColor(glm::vec3(0.8, 0.3, 1.0));
+		cylinder2.render(matrix2);
+
+		matrixs4 = glm::scale(matrixs4, glm::vec3(0.1f, 0.1f, 0.1f));
+		sphere.setProjectionMatrix(projection);
+		sphere.setViewMatrix(view);
+		sphere.enableWireMode();
+		cylinder.setColor(glm::vec3(0.8, 0.3, 1.0));
+		sphere.render(matrixs4);
+
+		matrix1 = glm::scale(matrix1, glm::vec3(0.15f, 0.8f, 0.15f));
+		cylinder.setProjectionMatrix(projection);
+		cylinder.setViewMatrix(view);
+		cylinder.enableWireMode();
+		cylinder.setColor(glm::vec3(0.1, 0.2, 0.4));
+		cylinder.render(matrix1);
+
+		matrixs2 = glm::scale(matrixs2, glm::vec3(0.1f, 0.1f, 0.1f));
+		sphere.setProjectionMatrix(projection);
+		sphere.setViewMatrix(view);
+		sphere.enableWireMode();
+		sphere.render(matrixs2);
+
+		matrixs3 = glm::scale(matrixs3, glm::vec3(0.1f, 0.1f, 0.1f));
+		sphere.setProjectionMatrix(projection);
+		sphere.setViewMatrix(view);
+		sphere.enableWireMode();
+		sphere.render(matrixs3);
+
+		glfwSwapBuffers(window);
+	}
+}
+
+int main(int argc, char ** argv) {
+	init(800, 700, "Window GLFW", false);
+	applicationLoop();
+	destroy();
+	return 1;
+}
---
+++ Practica7/15-Texturizado/src/main.cpp
@@ -0,0 +1,277 @@
+//glew include
+#include <GL/glew.h>
+
+//std includes
+#include <string>
+#include <iostream>
+
+//glfw include
+#include <GLFW/glfw3.h>
+
+//GLM include
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+// program include
+#include "Headers/TimeManager.h"
+#include "Headers/Shader.h"
+// Geometry primitives
+#include "Headers/Sphere.h"
+#include "Headers/Cylinder.h"
+#include "Headers/Box.h"
+#include "Headers/FirstPersonCamera.h"
+//Texture includes
+//Descomentar
+//#include "Headers/Texture.h"
+
+std::shared_ptr<FirstPersonCamera> camera(new FirstPersonCamera());
+
+Sphere sphere(20, 20);
+Sphere sphere2(20, 20);
+Cylinder cylinder(20, 20, 0.5, 0.5);
+Cylinder cylinder2(20, 20, 0.5, 0.5);
+Box box;
+
+Shader shader;
+//Descomentar
+//Shader shaderTexture;
+
+GLuint textureID1;
+
+int screenWidth;
+int screenHeight;
+
+GLFWwindow * window;
+
+bool exitApp = false;
+int lastMousePosX, offsetX;
+int lastMousePosY, offsetY;
+
+double deltaTime;
+
+// Se definen todos las funciones.
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes);
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod);
+void init(int width, int height, std::string strTitle, bool bFullScreen);
+void destroyWindow();
+void destroy();
+bool processInput(bool continueApplication = true);
+
+// Implementacion de todas las funciones.
+void init(int width, int height, std::string strTitle, bool bFullScreen) {
+
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		exit(-1);
+	}
+
+	screenWidth = width;
+	screenHeight = height;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	if (bFullScreen)
+		window = glfwCreateWindow(width, height, strTitle.c_str(),
+			glfwGetPrimaryMonitor(), nullptr);
+	else
+		window = glfwCreateWindow(width, height, strTitle.c_str(), nullptr,
+			nullptr);
+
+	if (window == nullptr) {
+		std::cerr
+			<< "Error to create GLFW window, you can try download the last version of your video card that support OpenGL 3.3+"
+			<< std::endl;
+		destroyWindow();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(0);
+
+	glfwSetWindowSizeCallback(window, reshapeCallback);
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Init glew
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		std::cerr << "Failed to initialize glew" << std::endl;
+		exit(-1);
+	}
+
+	glViewport(0, 0, screenWidth, screenHeight);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	glEnable(GL_DEPTH_TEST);
+
+	shader.initialize("../../Shaders/transformaciones.vs", "../../Shaders/transformaciones.fs");
+	//Descomentar
+	//shaderTexture.initialize("../../Shaders/texturizado.vs", "../../Shaders/texturizado.fs");
+
+	sphere.init();
+	sphere.setShader(&shader);
+	sphere.setColor(glm::vec3(0.3, 0.3, 1.0));
+
+	sphere2.init();
+	//Cambiar el objetos shader
+	sphere2.setShader(&shader);
+	sphere2.setColor(glm::vec3(0.3, 0.3, 1.0));
+	sphere2.scaleUVS(glm::vec2(2.0f, 2.0f));
+
+	cylinder.init();
+	cylinder.setShader(&shader);
+	cylinder.setColor(glm::vec3(0.8, 0.3, 1.0));
+
+	cylinder2.init();
+	cylinder2.setShader(&shader);
+	cylinder2.setColor(glm::vec3(0.2, 0.7, 0.3));
+
+	box.init();
+	box.setShader(&shader);
+	box.setColor(glm::vec3(0.2, 0.8, 0.4));
+
+	camera->setPosition(glm::vec3(0.0f, 0.0f, 0.4f));
+
+	// Descomentar
+	/*
+	int imageWidth, imageHeight;
+	Texture texture1("../../Textures/goku.png");
+	FIBITMAP* bitmap = texture1.loadImage();
+	unsigned char * data = texture1.convertToData(bitmap, imageWidth, imageHeight);
+	glGenTextures(1, &textureID1);
+	glBindTexture(GL_TEXTURE_2D, textureID1);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	if (data){
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_BGRA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+		std::cout << "Failed to load texture" << std::endl;
+	texture1.freeImage(bitmap);
+	*/
+
+}
+
+void destroyWindow() {
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void destroy() {
+	destroyWindow();
+
+	shader.destroy();
+	sphere.destroy();
+	cylinder.destroy();
+	cylinder2.destroy();
+}
+
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes) {
+	screenWidth = widthRes;
+	screenHeight = heightRes;
+	glViewport(0, 0, widthRes, heightRes);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (action == GLFW_PRESS) {
+		switch (key) {
+		case GLFW_KEY_ESCAPE:
+			exitApp = true;
+			break;
+		}
+	}
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+	offsetX = xpos - lastMousePosX;
+	offsetY = ypos - lastMousePosY;
+	lastMousePosX = xpos;
+	lastMousePosY = ypos;
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+		camera->mouseMoveCamera(offsetX, offsetY, deltaTime);
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
+	if (state == GLFW_PRESS) {
+		switch (button) {
+		case GLFW_MOUSE_BUTTON_RIGHT:
+			std::cout << "lastMousePos.y:" << lastMousePosY << std::endl;
+			break;
+		case GLFW_MOUSE_BUTTON_LEFT:
+			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
+			break;
+		case GLFW_MOUSE_BUTTON_MIDDLE:
+			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
+			std::cout << "lastMousePos.y:" << lastMousePosY << std::endl;
+			break;
+		}
+	}
+}
+
+bool processInput(bool continueApplication) {
+	if (exitApp || glfwWindowShouldClose(window) != 0) {
+		return false;
+	}
+	TimeManager::Instance().CalculateFrameRate(false);
+	deltaTime = TimeManager::Instance().DeltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera->moveFrontCamera(true, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera->moveFrontCamera(false, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera->moveRightCamera(false, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera->moveRightCamera(true, deltaTime);
+	glfwPollEvents();
+	return continueApplication;
+}
+
+void applicationLoop() {
+	bool psi = true;
+	double lastTime = TimeManager::Instance().GetTime();
+
+	while (psi) {
+		psi = processInput(true);
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+		// Matrix de proyeccion en perspectiva
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f),
+			(float)screenWidth / screenWidth, 0.01f, 100.0f);
+		// matrix de vista
+		glm::mat4 view = camera->getViewMatrix();
+
+		//Descomentar
+		//glBindTexture(GL_TEXTURE_2D, textureID1);
+		sphere2.setProjectionMatrix(projection);
+		sphere2.setViewMatrix(view);
+		sphere2.enableWireMode();
+		sphere2.setPosition(glm::vec3(0.0, 0.0, -4.0));
+		sphere2.render();
+		//Descomentar
+		//glBindTexture(GL_TEXTURE_2D, 0);
+
+		glfwSwapBuffers(window);
+	}
+}
+
+int main(int argc, char ** argv) {
+	init(800, 700, "Window GLFW", false);
+	applicationLoop();
+	destroy();
+	return 1;
+}
